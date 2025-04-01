4c7c074c2a2a8c7e8484d316f9a7f85ff4032389
alxpxx <51498212+alxpxx@users.noreply.github.com>
2025-04-02 05:14:26 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
 	name="insyst", 
-	version="0.1.0",
-	packages=find_packages(),
-	install_requires=[],
+	version="0.1",
+	packages=['insyst'],
+	install_requires=['iniconfig==2.1.0',
+		'packaging==24.2',
+		'pluggy==1.5.0',
+		'prompt_toolkit==3.0.50',
+		'pyfiglet==1.0.2',
+		'pytest==8.3.5',
+		'wcwidth==0.2.13'],
 	author="alxpx",
 	author_email="alxpx@outlook.com",
 	description="Displays information ahout the current system.",
 	long_description=open("README.md").read(),
-	long_description_content_type="text/markdown",
 	url="https://github.com/alxpxx/insyst",
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: Linux",
-	],
-	python_requires=">=3.6",
+	license='GNU',
 )
