f4071b7aa24db0eb84773b30ca7a835f5bd568fa
alxpxx <51498212+alxpxx@users.noreply.github.com>
2025-04-02 00:52:50 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
 	name="insyst", 
-	version="0.1.0"
+	version="0.1.0",
 	packages=find_packages(),
 	install_requires=[],
-	author="alxpx"
-	author_email="alxpx@outlook.com"
-	description="Displays information ahout the current system."
+	author="alxpx",
+	author_email="alxpx@outlook.com",
+	description="Displays information ahout the current system.",
 	long_description=open("README.md").read(),
 	long_description_content_type="text/markdown",
 	url="https://github.com/alxpxx/insyst",
